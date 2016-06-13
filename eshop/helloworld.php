a1630fb52536b59d36bed50c1c0ea3b2f00900a8
59763227 <59763227@qq.com>
2016-06-14 07:37:14 +0800

    eshop

---
+++ eshop/helloworld.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <meta charset="utf-8"/>
+        <title>Hello World</title>
+    </head>
+    <body>
+
+        <?php
+        echo "Hi, I'm a PHP script!";
+        ?>
+
+    </body>
+</html>
---
+++ eshop/index.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <meta charset="utf-8"/>
+        <title>E-Shop</title>
+    </head>
+    <body>
+
+        <?php
+        echo "Index";
+        ?>
+
+    </body>
+</html>
