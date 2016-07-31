934214b07d19a0bc3baa636111a1fd7dfcd4fb19
gavin <gavin2lee@163.com>
2016-07-31 19:50:44 +0800

    trans

--- exchange-center/src/main/java/com/gl/exchange/service/impl/ExchangeServiceImpl.java
+++ exchange-center/src/main/java/com/gl/exchange/service/impl/ExchangeServiceImpl.java
@@ -24,22 +24,22 @@ public class ExchangeServiceImpl implements ExchangeService {
 	@Transactional
 	public void transferCash(String fromHolder, String toHolder, double amount) {
 		log.debug(String.format("transfer cash %s from %s to %s", amount, fromHolder, toHolder));
-//		List<CashAccount> fromAccts = repo.findByHolder(fromHolder);
-//		if (fromAccts == null || fromAccts.size() < 1) {
-//			throw new RuntimeException();
-//		}
-//
-//		List<CashAccount> toAccts = repo.findByHolder(toHolder);
-//		if (toAccts == null || toAccts.size() < 1) {
-//			throw new RuntimeException();
-//		}
-		
+		// List<CashAccount> fromAccts = repo.findByHolder(fromHolder);
+		// if (fromAccts == null || fromAccts.size() < 1) {
+		// throw new RuntimeException();
+		// }
+		//
+		// List<CashAccount> toAccts = repo.findByHolder(toHolder);
+		// if (toAccts == null || toAccts.size() < 1) {
+		// throw new RuntimeException();
+		// }
+
 		CashAccount fromCa = repo.findByHolder(fromHolder);
 		CashAccount toCa = repo.findByHolder(toHolder);
-		
+
 		fromCa.setBalance(fromCa.getBalance() - amount);
 		toCa.setBalance(toCa.getBalance() + amount);
-		
+
 		repo.updateOne(fromCa);
 		repo.updateOne(toCa);
 	}
@@ -49,10 +49,10 @@ public class ExchangeServiceImpl implements ExchangeService {
 		log.debug(String.format("transfer security %s from %s to %s", shares, fromHolder, toHolder));
 		SecurityAccount fromAcct = dao.findByHolder(fromHolder);
 		SecurityAccount toAcct = dao.findByHolder(toHolder);
-		
+
 		fromAcct.setShares(fromAcct.getShares() - shares);
 		toAcct.setShares(toAcct.getShares() + shares);
-		
+
 		dao.updateOne(fromAcct);
 		dao.updateOne(toAcct);
 
@@ -61,25 +61,35 @@ public class ExchangeServiceImpl implements ExchangeService {
 	@Transactional
 	public void trade(String fromHolder, String toHolder, int shares, double price) {
 		log.debug(String.format("trade security %s by price %s from %s to %s", shares, price, fromHolder, toHolder));
-		SecurityAccount fromAcct = dao.findByHolder(fromHolder);
-		SecurityAccount toAcct = dao.findByHolder(toHolder);
-		
-		fromAcct.setShares(fromAcct.getShares() - shares);
-		toAcct.setShares(toAcct.getShares() + shares);
-		
-		dao.updateOne(fromAcct);
-		dao.updateOne(toAcct);
-		
-		double payment = price*shares;
-		
-		
+		SecurityAccount fromSa = dao.findByHolder(fromHolder);
+		SecurityAccount toSa = dao.findByHolder(toHolder);
+
+		toSa.setShares(toSa.getShares() + shares);
+		dao.updateOne(toSa);
+
+		if (fromSa.getShares() >= shares) {
+			fromSa.setShares(fromSa.getShares() - shares);
+		} else {
+			throw new RuntimeException("lack of security shares");
+		}
+
+		dao.updateOne(fromSa);
+
+		double payment = price * shares;
+
 		CashAccount fromCa = repo.findByHolder(fromHolder);
 		CashAccount toCa = repo.findByHolder(toHolder);
-		
+
 		fromCa.setBalance(fromCa.getBalance() + payment);
-		toCa.setBalance(toCa.getBalance() - payment);
-		
+
 		repo.updateOne(fromCa);
+
+		if (toCa.getBalance() >= payment) {
+			toCa.setBalance(toCa.getBalance() - payment);
+		} else {
+			throw new RuntimeException("lack of cash");
+		}
+
 		repo.updateOne(toCa);
 
 	}
@@ -101,11 +111,11 @@ public class ExchangeServiceImpl implements ExchangeService {
 	@Transactional
 	public CashAccount findCashAccountByHolder(String holder) {
 		log.debug(String.format("find %s by %s", CashAccount.class.getSimpleName(), holder));
-//		List<CashAccount> accts = repo.findByHolder(holder);
-//
-//		if (accts != null && accts.size() > 0) {
-//			return accts.get(0);
-//		}
+		// List<CashAccount> accts = repo.findByHolder(holder);
+		//
+		// if (accts != null && accts.size() > 0) {
+		// return accts.get(0);
+		// }
 
 		return repo.findByHolder(holder);
 	}
--- exchange-center/src/test/java/com/gl/exchange/service/ExchangeServiceTest.java
+++ exchange-center/src/test/java/com/gl/exchange/service/ExchangeServiceTest.java
@@ -10,14 +10,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.transaction.annotation.Transactional;
 
 import com.gl.exchange.model.CashAccount;
 import com.gl.exchange.model.SecurityAccount;
 
 @ContextConfiguration(locations = { "classpath:application-context.xml" })
 @RunWith(SpringJUnit4ClassRunner.class)
-@Transactional
+//@Transactional
 @Rollback(false)
 public class ExchangeServiceTest {
 	@Autowired
@@ -198,6 +197,61 @@ public class ExchangeServiceTest {
 		SecurityAccount storedSa = service.findSecurityAccountByHolder(holder);
 		MatcherAssert.assertThat(storedSa, CoreMatchers.notNullValue());
 		MatcherAssert.assertThat(storedSa.getHolder(), CoreMatchers.equalTo(holder));
+	}
+	
+	@Test
+	public void testTradeRollback() {
+		String holder1 = "a-rollback-"+System.nanoTime();
+		String holder2 = "b-rollback-"+System.nanoTime();
+		
+		int shares1 = 10000;
+		int shares2 = 20000;
+		
+		double balance1 = 10000.0;
+		double balance2 = 20000.0;
+		
+		SecurityAccount sa1 = new SecurityAccount();
+		sa1.setHolder(holder1);
+		sa1.setShares(shares1);
+		
+		SecurityAccount sa2 = new SecurityAccount();
+		sa2.setHolder(holder2);
+		sa2.setShares(shares2);
+		
+		service.addSecurityAccount(sa1);
+		service.addSecurityAccount(sa2);
+		
+		CashAccount ca1 = new CashAccount();
+		ca1.setHolder(holder1);
+		ca1.setBalance(balance1);
+		
+		CashAccount ca2 = new CashAccount();
+		ca2.setHolder(holder2);
+		ca2.setBalance(balance2);
+		
+		service.addCashAccount(ca1);
+		service.addCashAccount(ca2);
+		
+		service.trade(holder1, holder2, 100, 1200.0);
+		
+		
+		CashAccount storedCa1 = service.findCashAccountByHolder(holder1);
+		CashAccount storedCa2 = service.findCashAccountByHolder(holder2);
+		
+		MatcherAssert.assertThat(storedCa1, CoreMatchers.notNullValue());
+		MatcherAssert.assertThat(storedCa2, CoreMatchers.notNullValue());
+		
+		MatcherAssert.assertThat(storedCa1.getBalance(), Matchers.greaterThan(ca1.getBalance()));
+		MatcherAssert.assertThat(storedCa2.getBalance(), Matchers.lessThan(ca2.getBalance()));
+		
+		SecurityAccount storedSa1 = service.findSecurityAccountByHolder(holder1);
+		SecurityAccount storedSa2 = service.findSecurityAccountByHolder(holder2);
+		
+		MatcherAssert.assertThat(storedSa1, CoreMatchers.notNullValue());
+		MatcherAssert.assertThat(storedSa2, CoreMatchers.notNullValue());
+		
+		MatcherAssert.assertThat(storedSa1.getShares(), Matchers.lessThan(sa1.getShares()));
+		MatcherAssert.assertThat(storedSa2.getShares(), Matchers.greaterThan(sa2.getShares()));
 	}
 
 }
