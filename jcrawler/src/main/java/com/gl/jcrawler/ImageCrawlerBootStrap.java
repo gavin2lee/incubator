3cf35d7a59f3547f49c672b90e2d4bb2ea5da425
59763227 <59763227@qq.com>
2016-08-21 18:40:31 +0800

    crawler

---
+++ jcrawler/src/main/java/com/gl/jcrawler/ImageCrawlerBootStrap.java
@@ -0,0 +1,10 @@
+package com.gl.jcrawler;
+
+public class ImageCrawlerBootStrap {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ jcrawler/src/test/java/com/gl/jcrawler/BootTest.java
@@ -0,0 +1,14 @@
+package com.gl.jcrawler;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BootTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
