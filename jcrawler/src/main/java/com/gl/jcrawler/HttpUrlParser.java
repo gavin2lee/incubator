3df0b4319dd70c7f406c77ce78561bdc6795d71b
59763227 <59763227@qq.com>
2016-08-22 17:49:08 +0800

    crawler

---
+++ jcrawler/src/main/java/com/gl/jcrawler/HttpUrlParser.java
@@ -0,0 +1,5 @@
+package com.gl.jcrawler;
+
+public class HttpUrlParser {
+
+}
---
+++ jcrawler/src/main/java/com/gl/jcrawler/LinkQueue.java
@@ -0,0 +1,47 @@
+package com.gl.jcrawler;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class LinkQueue {
+	private Set<String> visitedUrls = Collections.synchronizedSet(new HashSet<String>());
+	private List<String> unvisitedUrls = Collections.synchronizedList(new ArrayList<String>());
+	
+	public String popUnvisitedUrls(){
+		if(!unvisitedUrls.isEmpty()){
+			String url = unvisitedUrls.remove(0);
+			visitedUrls.add(url);
+			
+			return url;
+		}
+		
+		return null;
+	}
+	
+	public void addVisitedUrls(String url){
+		if(url == null){
+			return;
+		}
+		
+		if(url.trim().length() < 1){
+			return;
+		}
+		
+		if(visitedUrls.contains(url)){
+			return;
+		}
+		
+		if(unvisitedUrls.contains(url)){
+			return;
+		}
+		
+		unvisitedUrls.add(url);
+	}
+	
+	public boolean isUnvisitedUrlsEmpty(){
+		return unvisitedUrls.isEmpty();
+	}
+}
